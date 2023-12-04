b99c9bdd666fa8d621f87b43a302b88fffc02b23
Matteo Di Noia <dmatteo002@gmail.com>
2023-12-04 18:41:12 +0100

    Renaming file and creating basic logger interface

--- server/src/main/java/frontend/LogController.java
+++ server/src/main/java/frontend/LogController.java
@@ -1,9 +1,9 @@
-package server.server;
+package frontend;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LogController {
     @FXML
     private Label welcomeText;
 
