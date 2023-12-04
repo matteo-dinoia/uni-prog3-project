71132b75e3e88c0642070b6f16bb5c5d9b7ee242
Matteo Di Noia <dmatteo002@gmail.com>
2023-12-04 21:16:12 +0100

    Adding base for running server

--- server/src/main/java/frontend/App.java
+++ server/src/main/java/frontend/App.java
@@ -1,20 +1,42 @@
 package frontend;
 
+import backend.Server;
+import interfaces.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
 public class App extends Application {
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 400;
+    private Stage stage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("log-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage st) throws IOException {
+        this.stage = st;
+
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("log-view.fxml"));
+        stage.setScene(new Scene(loader.load(), WIDTH, HEIGHT));
+
+        setupServer(loader.getController());
+        setParameters();
+    }
+
+    private void setParameters(){
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+
+        stage.setMinWidth(WIDTH);
+        stage.setMinHeight(HEIGHT);
         stage.show();
+    }
+
+    private void setupServer(Logger logger){
+        Server server = Server.getServer(logger);
+        server.start();
     }
 
     public static void main(String[] args) {
--- server/src/main/java/frontend/LogController.java
+++ server/src/main/java/frontend/LogController.java
@@ -1,14 +1,17 @@
 package frontend;
 
+import interfaces.Logger;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
-public class LogController {
-    @FXML
-    private Label welcomeText;
+public class LogController implements Logger {
+    @FXML private TextArea logText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML private void cleanOutput() {
+        logText.setText("");
+    }
+
+    public void log(String str){
+        logText.appendText(str + "\n");
     }
 }
