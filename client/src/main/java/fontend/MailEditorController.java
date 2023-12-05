9a54dd461b548c95154c5b274c5bc28da7eceeed
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-12-05 15:20:03 +0100

    starting implementation of cancel and send methods

--- client/src/main/java/fontend/MailEditorController.java
+++ client/src/main/java/fontend/MailEditorController.java
@@ -2,13 +2,18 @@ package fontend;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MailEditorController {
 
     @FXML private void cancel(ActionEvent actionEvent) {
-
+        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
     }
 
     @FXML private void send(ActionEvent actionEvent) {
+        // TODO fix this
+        cancel(actionEvent);
     }
 }
