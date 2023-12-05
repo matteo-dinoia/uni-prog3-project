0dd30b7937e29c1dee4954a64a98d601ec30363a
Alexander Delsanto <76659093+TeaBit76@users.noreply.github.com>
2023-12-05 11:59:21 +0100

    First implementation of mail editor GUI and MailEditorController class (#4)
    * creating first version of mail-editor-view
    
    * beginning implementation of MailEditorController
    
    * Style change and remove useless properties
    
    ---------
    
    Co-authored-by: Matteo <73781428+matteo-dinoia@users.noreply.github.com>

---
+++ client/src/main/java/fontend/MailEditorController.java
@@ -0,0 +1,14 @@
+package fontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MailEditorController {
+
+    @FXML private void cancel(ActionEvent actionEvent) {
+
+    }
+
+    @FXML private void send(ActionEvent actionEvent) {
+    }
+}
