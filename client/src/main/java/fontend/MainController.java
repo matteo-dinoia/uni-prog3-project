4ec7d5476f3c90a056447ecd6c2ca69f83f14168
TeaBit76 <alexander.delsanto@edu.unito.it>
2023-12-05 15:23:22 +0100

    removing unused import

--- client/src/main/java/fontend/MainController.java
+++ client/src/main/java/fontend/MainController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
